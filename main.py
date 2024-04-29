0f4f5834a5b9085d7482da2fec67f437114c78ce
kenkkui <thifirstime@gmail.com>
2024-04-29 09:31:56 +0800

    just a commit

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 1000
-MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine(rows, cols, symbols):
+    all_symbol = []
+
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbol.apppend(symbol)
+
+    columns = [[], [], []]
+    for col in range(cols):
+        column = []
+        for row in range(rows):
+            value = random,choice(all_symbol)
+
+
 
 def deposit():
     while True:
-        amount = input("What woud you like to deposit? $ ")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -35,7 +61,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        amount = input("What woud you like to bet on each line? $ ")
+        amount = input("What would you like to bet on each line? $ ")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
@@ -53,8 +79,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+
+    while True: 
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 main()
